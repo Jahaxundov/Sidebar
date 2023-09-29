c4e8948b8f24ed5eda8013260e2a1cb599eb506c
Jahaxundov <jahangirea@code.edu.az>
2023-09-29 17:09:25 +0400

    Added

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,7 @@
 //     i.onclick = function () {
 //         li.remove();
 //     }
+
 // })
 
 
@@ -37,22 +38,26 @@
 
 // console.log(document.querySelector("h1").closest("container"));
 
-// let sidebar=document.querySelector(".sidebar");
-
-// let openIcon=document.querySelector(".sidebar .open-icon");
-
-// let closeIcon=document.querySelector(".sidebar .close-icon");
 
 
-// openIcon.addEventListener("click", function(){
-//     sidebar.classList.remove("move-sidebar")
-//     this.previousElementSibling.classList.remove("d-none")
-//     this.classList.add("d-none")
-// })
 
-// closeIcon.addEventListener("click", function(){
-//     sidebar.classList.add("move-sidebar")
-//     this.nextElementSibling.classList.remove("d-none")
-//     this.classList.add("d-none")
 
-// })
+let sidebar=document.querySelector(".sidebar");
+
+let openIcon=document.querySelector(".sidebar .open-icon");
+
+let closeIcon=document.querySelector(".sidebar .close-icon");
+
+
+openIcon.addEventListener("click", function(){
+    sidebar.classList.remove("move-sidebar")
+    this.previousElementSibling.classList.remove("d-none")
+    this.classList.add("d-none")
+})
+
+closeIcon.addEventListener("click", function(){
+    sidebar.classList.add("move-sidebar")
+    this.nextElementSibling.classList.remove("d-none")
+    this.classList.add("d-none")
+
+})
